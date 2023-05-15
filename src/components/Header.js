6b8552f977edb1441205aa9bc916c928a0690c0c
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-15 22:14:39 +0900

    feat: 헤더 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import fontawesome from '@fortawesome/fontawesome';
-import { faCheckSquare, faCoffee } from '@fortawesome/fontawesome-free-solid';
+import {
+  faCheckSquare,
+  faCoffee,
+  faSearch,
+} from '@fortawesome/fontawesome-free-solid';
 
 import '../styles/components/Header.scss';
 
-fontawesome.library.add(faCheckSquare, faCoffee);
+fontawesome.library.add(faCheckSquare, faCoffee, faSearch);
 
 const Header = () => {
   return (
     <div id='header-wrapper'>
-      <div>
-        <FontAwesomeIcon icon='fa-solid fa-bars' id='bars-icon' />
+      <div className='bars-wrapper'>
+        <FontAwesomeIcon icon='fa-solid fa-bars' id='header-icon' />
+      </div>
+      <h2 className='title'>화슐랭</h2>
+      <div className='search-wrapper'>
+        <FontAwesomeIcon icon='search' id='header-icon' />
       </div>
     </div>
   );
