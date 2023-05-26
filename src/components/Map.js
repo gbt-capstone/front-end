6c49535effab99cbee0f3000c5fb0cef3db2842d
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-05-26 16:54:53 +0900

    fix: 화장실 위도,경도 데이터 바뀌어 저장된 오류 개선

--- src/components/Map.js
+++ src/components/Map.js
@@ -49,8 +49,8 @@ const Map = () => {
           let position = {
             title: toilet.name,
             latlng: new kakao.maps.LatLng(
-              response.data.documents[0].x,
-              response.data.documents[0].y
+              response.data.documents[0].y,
+              response.data.documents[0].x
             ),
           };
 
@@ -109,14 +109,13 @@ const Map = () => {
     positions.forEach((position) => {
       let marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
-        position: position.latlng, // 마커를 표시할 위치
+        position: position.position, // 마커를 표시할 위치
         title: position.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         image: position.image, // 마커 이미지
       });
-
-      marker.setMap(map);
-
+      // marker.setMap(map);
       console.log(marker);
+      // console.log(map);
     });
   };
 
